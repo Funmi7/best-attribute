065b7bf4363f79b8fbe2a0bf1db5217ccd71d221
funtee7 <talabifunmilayo@gmail.com>
2021-07-28 21:07:17 +0100

    added some of the data

---
+++ src/data/Data.js
@@ -0,0 +1,82 @@
+export const data = [
+  {
+    options: [
+      { option: "Adventorous", personality: "Leader" },
+      { option: "Adaptable", personality: "Analyst" },
+      { option: "Animated ", personality: "Carefree" },
+      { option: "Analytical", personality: "Workaholic" },
+    ],
+  },
+  {
+    options: [
+      { option: "Persistent", personality: "Workaholic" },
+      { option: "Playful", personality: "Carefree" },
+      { option: "Persuasive", personality: "Leader" },
+      { option: "Peaceful", personality: "Analyst" },
+    ],
+  },
+  {
+    options: [
+      { option: "Submissive", personality: "Analyst" },
+      { option: "Self sacrificing", personality: "Workaholic" },
+      { option: " Sociable", personality: "Carefree" },
+      { option: "Strong willed", personality: "Leader" },
+    ],
+  },
+  {
+    options: [
+      { option: "Considerate", personality: "Workaholic" },
+      { option: "Controlled", personality: "Analyst" },
+      { option: " Competitive", personality: "Leader" },
+      { option: "Convincing", personality: "Carefree" },
+    ],
+  },
+  {
+    options: [
+      { option: "Refreshing", personality: "Carefree" },
+      { option: "Respectful", personality: "Workaholic" },
+      { option: "Reserved	", personality: "Analyst" },
+      { option: "Resourceful", personality: "Leader" },
+    ],
+  },
+  {
+    options: [
+      { option: "Satisfied", personality: "Analyst" },
+      { option: "Sensitive", personality: "Workaholic" },
+      { option: "Self reliant", personality: "Leader" },
+      { option: "Spirited", personality: "Carefree" },
+    ],
+  },
+  {
+    options: [
+      { option: "Planner ", personality: "Workaholic" },
+      { option: "Patient", personality: "Analyst" },
+      { option: "Positive", personality: "Leader" },
+      { option: "Promoter", personality: "Carefree" },
+    ],
+  },
+  {
+    options: [
+      { option: "Sure", personality: "Leader" },
+      { option: "Spontaneous", personality: "Carefree" },
+      { option: "Scheduled", personality: "Workaholic" },
+      { option: "Shy", personality: "Analyst" },
+    ],
+  },
+  {
+    options: [
+      { option: "Orderly ", personality: "Workaholic" },
+      { option: "Obliging", personality: "Analyst" },
+      { option: "Outspoken", personality: "Leader" },
+      { option: "Optimistic", personality: "Carefree" },
+    ],
+  },
+  {
+    options: [
+      { option: "Friendly", personality: "Analyst" },
+      { option: "Faithful", personality: "Workaholic" },
+      { option: "Funny", personality: "Carefree" },
+      { option: "Forceful	", personality: "Leader" },
+    ],
+  },
+];
